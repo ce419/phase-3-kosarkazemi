d258c62cbf95b9fed4b3df6a17ea96c04e834de3
khosh <Kosarkazemi588@gmail.com>
2017-07-10 05:01:39 +0430

    1

--- author/serializers.py
+++ author/serializers.py
@@ -6,4 +6,4 @@ from .models import User,Blog
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('std_num','password' , 'first_name', 'last_name', 'email') #TODO
+        fields = ('std_num','password' , 'first_name',  'last_name', 'email') #TODO
