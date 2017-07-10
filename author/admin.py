1cf54ad1f34a040809090e8335bd4d7f6de24fdc
khosh <Kosarkazemi588@gmail.com>
2017-07-10 04:30:19 +0430

    1

--- author/admin.py
+++ author/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import User,Blog
 
-admin.site.register(User)
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['id','std_num','password' , 'first_name', 'last_name', 'email']
+
+
+admin.site.register(User,UserAdmin)
 admin.site.register(Blog)
