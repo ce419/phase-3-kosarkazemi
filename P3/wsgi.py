a8e3d85aae047415ab2a52466cad078a3a38df7d
kosarkazemi <kosarkazemi588@gmail.com>
2017-07-23 17:29:26 +0430

    fix

--- P3/wsgi.py
+++ P3/wsgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "P3-c.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "P3.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
